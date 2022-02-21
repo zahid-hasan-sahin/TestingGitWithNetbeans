5ca108416700a5e5131a7d5012fe51d3e9059b44
zahid hasan <zahidhasan6112@gmail.com>
2022-02-21 23:06:38 +0600

    First Commit

---
+++ src/testing_git/Testing_Git.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testing_git;
+
+/**
+ *
+ * @author Zahid
+ */
+public class Testing_Git {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hellow git");
+    }
+
+}
