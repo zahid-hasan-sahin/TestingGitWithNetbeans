ff1ab553b19b94c6ee41bff7483f98ace975df58
zahid hasan <zahidhasan6112@gmail.com>
2022-02-22 09:33:39 +0600

    secound Commit from newB

--- src/testing_git/Testing_Git.java
+++ src/testing_git/Testing_Git.java
@@ -14,7 +14,8 @@ public class Testing_Git {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hellow git");
+        System.out.println("Hellow git");      
+        System.out.println("From NewB");
     }
 
 }
